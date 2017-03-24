9b5277c18bb7e0be08ad91cb358ee187b3a5b7cf
githubziven <1240500584@qq.com>
2017-03-24 18:00:17 +0800

    动态调整侧边栏高宽

--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -7,9 +7,9 @@
         </div>
         <!-- 侧边栏 -->
         <div class="container">
-            <el-row class="container-row" >
-                <el-col class="slide-navbar" :span="3" >
-                    <el-menu default-active="2" :style="{height:heightSize+'px'}" @open="handleOpen" @close="handleClose">
+            <el-row class="container-row">
+                <el-col class="slide-navbar" :span="3">
+                    <el-menu default-active="2" :style="{height:heightSize+'px',minWidth:widthSize+'px'}" @open="handleOpen" @close="handleClose">
                         <el-submenu index="1">
                             <template slot="title"><i class="el-icon-message"></i>导航一</template>
                             <el-menu-item-group>
@@ -39,12 +39,14 @@
 <script>
 export default {
     data() {
-            return {}
+            return {
+                widthSize:120
+            }
         },
         computed: {
             heightSize: function() {
                 //对于绝大部分浏览器使用window.innerWidth即可获取宽度，使用document.documentElement.clientWidth || document.body.clientWidth 为了实现对IE6，7的支持
-                return (window.innerHeight-60) || (document.documentElement.clientHeight-60) || (document.body.clientHeight-60)
+                return (window.innerHeight - 60) || (document.documentElement.clientHeight - 60) || (document.body.clientHeight - 60)
             }
         },
         methods: {
@@ -70,6 +72,15 @@ export default {
             background-image: url('../../assets/imgs/top-logo.png');
             margin-top: 14px;
             margin-left: 60px;
+        }
+        .container {
+            .container-row {
+                .slide-navbar {
+                    ul.el-menu {
+                        min-width: 120px;
+                    }
+                }
+            }
         }
         // .no-svg .statu-navbar__logo{
         //  background: url()
